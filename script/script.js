7a7525b87da2ea35f0bc1674a0b4cf37bc8aee69
Elder1360 <mohamm60@outlook.com>
2019-09-18 23:09:02 +0430

    first

---
+++ script/script.js
@@ -0,0 +1,33 @@
+/*
+# ------------------------------------------------------------------------
+# Free Slide SP1 - Slideshow module for Joomla 2.5
+# ------------------------------------------------------------------------
+# Copyright (C) 2010 - 2012 JoomShaper.com. All Rights Reserved.
+# @license - GNU/GPL, see LICENSE.php,
+# Author: JoomShaper.com
+# Websites:  http://www.joomshaper.com
+# ------------------------------------------------------------------------
+*/
+eval(function(p, a, c, k, e, r) {
+    e = function(c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    }
+    ;
+    if (!''.replace(/^/, String)) {
+        while (c--)
+            r[e(c)] = k[c] || e(c);
+        k = [function(e) {
+            return r[e]
+        }
+        ];
+        e = function() {
+            return '\\w+'
+        }
+        ;
+        c = 1
+    }
+    ;while (c--)
+        if (k[c])
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b','g'), k[c]);
+    return p
+}('q 1V=I 22({20:[1E,1W],m:{6:\'1D-13-X\',C:{16:1B,17:1G},M:1A,Q:{}},1F:7(a,b){2.Y(b);2.w=$(a);2.4=2.w.1J();3(2.4.8>1){2.s=0;2.v=5;2.V=5;2.T=5;2.11=5;2.6=5;2.u=5;2.B=[];2.N={};2.1r();2.E(\'29\')}},1r:7(){q a={y:\'R\',1k:0,1i:0},b;2.w.A(1P.1S(a,{1T:\'1U\'}));3(2.w.23(\'O\')!=\'1f\'){2.w.9(\'O\',\'1C\')}p(b=0;b<2.4.8;b++){2.4[b].A(a);2.B[b]=5}2.S=7(){3(2.v!==5){2.4[2.v].9(\'y\',\'K\')}}.14([],2);2.1l(2.m.6)},1o:7(a){q b={16:\'P\',17:\'1j\'},c,d,e;3(2.u){2.u.G()}p(c 1z a){d=a[c];p(e=0;e<2.4.8;e++){2.4[e].9(c,d+\'L\')}2.w.9(c,d+\'L\');3(2.6.D==\'15\'){3(2.6.r==b[c]){d*=2.4.8}2.n.9(c,d+\'L\')}}2.m.C=a;2.19(2.s,2.v,H)},1l:7(a){q b;a=a.1H(\'-\').1I([\'D\',\'r\',\'t\']);3(!a.t){a.t=\'\'}3(2.6===5||2.6.D!=a.D||2.6.r!=a.r||2.6.t!=a.t){3(a.D==\'15\'){3(!2.n){2.n=I 1K(\'1L\');p(b=0;b<2.4.8;b++){2.4[b].A({\'y\':\'R\',\'O\':\'1M\',\'x\':0,\'F\':0,\'U\':1,\'1c\':\'K\'}).18(2.n)}2.n.18(2.w);2.n.A({O:\'1f\',1k:0,1i:0,x:0,F:0});2.u=I 12.1Z(2.n)}o 3(2.u){2.u.G()}3(a.r==\'P\'){2.n.9(\'F\',0)}o{2.n.9(\'x\',0)}3(a.r==\'P\'){p(b=0;b<2.4.8;b++){2.4[b].9(\'1c\',\'x\')}}}o{3(2.n){p(b=0;b<2.4.8;b++){2.4[b].18(2.w)}2.n.21();2.n=5}p(b=0;b<2.4.8;b++){2.4[b].A({\'U\':1,\'O\':\'1f\',\'1c\':\'K\'})}}2.6=a;2.1o(2.m.C)}},19:7(a,b,c){q d,e,g,h,l={P:[\'x\',\'16\'],1j:[\'F\',\'17\']},f=l[2.6.r],k,i,j;3(2.6.D==\'15\'){k=2.m.C[f[1]]*b*-1;i=2.m.C[f[1]]*a*-1;3(c){2.n.9(f[0],i+\'L\')}o{g=2.1q().G();g.Z(f[0],k,i)}}o{d=2.4[a];e=2.4[b];p(j=0;j<2.4.8;j++){3(a===j){2.4[j].A({\'y\':\'R\',\'z-1s\':1,\'x\':0,\'F\':0});3(2.6.r==\'13\'&&2.6.t==\'X\'){2.4[j].1a(0)}}o 3(b===j){2.4[j].A({\'y\':\'R\',\'z-1s\':0,\'x\':0,\'F\':0})}o{2.4[j].9(\'y\',\'K\')}}3(2.6.r==\'13\'){3(2.6.t==\'X\'){g=2.1b(a).G();3(c){d.1a(1)}o{g.10(\'1d\',2.S);g.Z({U:[0,1]})}}o{3(b!==5){2.4[b].9(\'y\',\'K\')}}}o{k=2.m.C[f[1]];i=0;3(a<b){k*=-1}3(c){d.9(f[0],i)}o{d.9(f[0],k+\'L\');e.9(f[0],i);3(2.6.t==\'1m\'){g=2.1n([a,b]).G();h={\'0\':{}};h[\'0\'][f[0]]=[k,i];3(e){h[\'1\']={};h[\'1\'][f[0]]=[i,k*-1]}g.Z(h)}o{g=2.1b(a).G();g.10(\'1d\',2.S);h={};h[f[0]]=[k,i];3(2.6.t==\'X\'){d.1a(0);h.U=[0,1]}g.Z(h)}}}}},1b:7(a){3(!2.B[a]){2.B[a]=I 12.1N(2.4[a])}2.B[a].1O(\'1d\',2.S).Y(2.m.Q);1e 2.B[a]},1n:7(a){q b=a.1Q(\'1R\'),c=[],d;3(!2.N[b]){p(d=0;d<a.8;d++){3(a[d]!==5){c.1m(2.4[a[d]])}}2.N[b]=I 12.1p(I 1p(c))}2.N[b].Y(2.m.Q);1e 2.N[b]},1q:7(a){2.u.Y(2.m.Q);1e 2.u},W:7(a,b,c){3(a!==2.s){2.v=2.s;2.s=a;q d=5;3(c!==H){3(2.T){d=\'1g\'}o 3(2.11){d=\'1h\'}3(d){2.J()}}2.E(\'1X\',[2.s,2.v]);2.19(a,2.v,b);3(d){2[d]()}2.E(\'1Y\',[2.s,2.v])}},1t:7(a){q b=2.s+1;3(b===2.4.8){b=0}2.W(b,5,a)},1u:7(a){q b=2.s-1;3(b<0){b=2.4.8-1}2.W(b,5,a)},1g:7(a){2.J();3(a&&1v(a)==\'1w\'){2.m.M=a}2.T=H;2.V=2.1t.1x(2.m.M,2,[H]);2.E(\'24\',[\'1g\'])},1h:7(a){2.J();3(a&&1v(a)==\'1w\'){2.m.M=a}2.11=H;2.V=2.1u.1x(2.m.M,2,[H]);2.E(\'25\',[\'1h\'])},J:7(){26(2.V);2.T=5;2.11=5;2.E(\'27\',[\'J\'])},28:7(a,b,c){3(!c){c=\'1y\'}p(q d=0;d<b.8;d++){b[d].10(c,2[a].14([5],2))}},2a:7(a,b){3(!b){b=\'1y\'}p(q c=0;c<a.8;c++){a[c].10(b,2.W.14([c,5,5],2))}}});', 62, 135, '||this|if|items|false|transition|function|length|setStyle|||||||||||||options|subcontainer|else|for|var|direction|current_item_index|additional|fx|preceding_item_index|container|left|display||setStyles|fxs|size|type|fireEvent|top|cancel|true|new|stop|none|px|interval|fxs_mix|position|horizontal|fxOptions|block|hidePrecedingitem|play_right_running|opacity|play_timer|walk|fade|setOptions|start|addEvent|play_left_running|Fx|inplace|pass|slide|width|height|inject|displayItem|setOpacity|getItemFx|float|complete|return|absolute|play|playback|padding|vertical|margin|setTransition|push|getItemsFx|setSize|Elements|getSlideFx|build|index|next|previous|typeOf|number|periodical|click|in|5000|640|relative|cover|Options|initialize|360|split|associate|getChildren|Element|div|static|Morph|removeEvent|Object|join|_|merge|overflow|hidden|freeSlide_sp1|Events|onBeforeDisplayItem|onWalk|Tween|Implements|dispose|Class|getStyle|onStartPlay|onStartPlayback|setTimeout|onStop|addPlayerControls|onInitialized|addItemWalkers'.split('|'), 0, {}))
